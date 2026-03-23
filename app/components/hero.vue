8276cb8a5b3d5d241f2a19c2fd68fc0b0e4ce6b8
charlieweb <charlyesp82@gmail.com>
2026-03-23 13:25:38 -0600

    update config files

---
+++ app/components/hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-200 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and share your experiences with friends and family. Create a travel log to document your adventures and inspire others to explore the world.
+        </p>
+        <button class="btn btn-accent">
+          Sign In with GitHub
+          <Icon name="tabler:brand-github" size="32" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
+import "./lib/env";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  css: ["./app/assets/css/main.css"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+  ],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  vite: {
+    plugins: [tailwindcss() as any],
+  },
+  colorMode: {
+    dataValue: "theme",
   },
 });
