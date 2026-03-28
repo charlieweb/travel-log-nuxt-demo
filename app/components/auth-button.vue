a2b7cdacff48ffe2a90d42a0dda3fdcc4b9bb497
charlieweb <charlyesp82@gmail.com>
2026-03-27 19:02:51 -0600

    add logout link to the page

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -3,7 +3,25 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <button :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
+  <div v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+    <div tabindex="0" role="button" class="btn m-1">
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </div>
+    <ul tabindex="-1" class="dropdown-content menu bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/sign-out" class="flex items-center gap-2 text-base-content">
+          Sign Out
+          <Icon name="tabler:logout-2" size="23" />
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+  <button v-else :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
     Sign in With GitHub
     <span v-if="authStore.loading" class="loading loading-spinner loading-xs" />
     <Icon v-else name="tabler:brand-github" size="32" />
---
+++ app/pages/sign-out.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="card  bg-base-300 container  mt-4 min-h-72 text-center mx-auto flex flex-col justify-center items-center gap-4">
+    <span v-if="authStore.loading" class="loading loading-spinner loading-xs" />
+  </div>
+</template>
