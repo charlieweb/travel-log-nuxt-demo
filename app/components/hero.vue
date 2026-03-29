37ba69b947ec3c3634d2f4bb56a93705aa82ea2d
charlieweb <charlyesp82@gmail.com>
2026-03-28 22:44:10 -0600

    update store to run with ssr

--- app/components/hero.vue
+++ app/components/hero.vue
@@ -1,3 +1,7 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero bg-base-200 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
@@ -8,7 +12,11 @@
         <p class="py-6">
           Keep track of your travels and share your experiences with friends and family. Create a travel log to document your adventures and inspire others to explore the world.
         </p>
-        <AuthButton />
+
+        <AuthButton v-if="!authStore.user" />
+        <NuxtLink v-else to="/dashboard" class="btn btn-primary">
+          Start Logging Your Travels
+        </NuxtLink>
       </div>
     </div>
   </div>
